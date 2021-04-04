b67e18a561ee3170ae44b6651f38f51ab9ff73cd
Fatimah Emad El-Den <63984422+xxfatj@users.noreply.github.com>
2021-04-04 07:35:58 +0300

    Add files via upload
    In the main-method create a new HashMap<String,String> object. Store the names and nicknames of the following people in this hashmap so, that the name is the key and the nickname is the value. Use only lower case letters.
    
    matthew's nickname is matt
    michael's nickname is mix
    arthur's nickname is artie
    Then get Matthew's nickname from the hashmap, and print it.
    
    There is no automated tests for this exercise. Just submit the exercise when you think it works as it should.

---
+++ part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -0,0 +1,16 @@
+
+import java.util.HashMap;
+
+public class Nicknames {
+
+    public static void main(String[] args) {
+        // Do the operations required here!
+        
+        HashMap <String, String> Nicknames = new HashMap<>();
+Nicknames.put("matthew", "matt");
+Nicknames.put("micheal", "mix");
+Nicknames.put("arthur", "artie");
+System.out.println(Nicknames.get("matthew"));
+    }
+
+}
